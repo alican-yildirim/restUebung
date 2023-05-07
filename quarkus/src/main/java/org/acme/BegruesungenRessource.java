324da0f82dfd92705ca8721f35ed93970bbdf223
alican-yildirim <101459010+alican-yildirim@users.noreply.github.com>
2023-05-07 22:09:01 +0200

    Versucht PG-Datenbank Verbindung herzustellen. Mit Docker Image gestartet und pg-client vert.x versucht. Leider nicht zu Ende gebracht.

--- quarkus/src/main/java/org/acme/BegruesungenRessource.java
+++ quarkus/src/main/java/org/acme/BegruesungenRessource.java
@@ -1,18 +1,52 @@
 package org.acme;
 
 import io.vertx.pgclient.PgPool;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
+import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
 import org.openapi.quarkus.openapi_yaml.api.BegruesungenApi;
 import org.openapi.quarkus.openapi_yaml.model.Namen;
 
 public class BegruesungenRessource implements BegruesungenApi {
 
+    private static String res;
     @Inject
     PgPool client;
 
+    @PostConstruct
+    void config(){
+        initDb();
+    }
+
+    private void initDb(){
+//                .flatMap(m-> client.query("CREATE TABLE hallo (id SERIAL PRIMARY KEY, title TEXT NOT NULL)")).execute();
+        this.client.query("DROP TABLE IF EXISTS hallo").execute();
+        this.client.query("CREATE TABLE hallo (id SERIAL PRIMARY KEY, title TEXT NOT NULL)");
+        this.client.query("INSERT INTO hallo (title) VALUES ('HALLO')").execute();
+        this.client.query("INSERT INTO hallo (title) VALUES ('SERVUS')").execute();
+    }
+
+    public static String findAll(PgPool PgClient){
+
+        PgClient.query("SELECT id, title FROM hallo ORDER BY title DESC").execute(ar -> {
+            if (ar.succeeded()) {
+                RowSet<Row> rows = ar.result();
+                for (Row row : rows) {
+                    res = row.getString(0) + " " + row.getString(1);
+                }
+            } else {
+                res =  "Failure: " + ar.cause().getMessage();
+            }
+        });
+
+        return res;
+    }
+
     @Override
     public String gruesenGet(String vorname, String nachname) {
-        return "Hallo " + vorname + " " + nachname + "!";
+//        return "Hallo " + vorname + " " + nachname + "!";
+        return findAll(this.client);
     }
 
     @Override
